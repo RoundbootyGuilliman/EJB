79ca633bf3c10faa223aa333b2ebdd6be32f5700
Kristian_Koshaliyev <Kristian_Koshaliyev@epam.com>
2018-07-31 14:25:08 +0600

    [IMP] ТЕСТ ХУЕСТ

---
+++ src/main/java/app/service/impl/Calculator.java
@@ -0,0 +1,17 @@
+package app.service.impl;
+
+public class Calculator {
+	
+	public int getSum(int x, int y) {
+		return x + y;
+	}
+	
+	public int getDivide(int x, int y) {
+		return x / y;
+	}
+	
+	public int getMultiple(int x, int y) {
+		return x * y;
+	}
+	
+}
---
+++ src/test/java/app/service/impl/NewsServiceImplTest.java
@@ -0,0 +1,48 @@
+package app.service.impl;
+
+import app.dao.impl.NewsDAOImpl;
+import app.entity.News;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Date;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class NewsServiceImplTest {
+	
+	@Mock
+	NewsDAOImpl dao;
+	
+	@InjectMocks
+	NewsServiceImpl testedClass;
+	
+	News news;
+	
+	@Before
+	public void initTest() {
+		news = new News();
+		news.setAuthor("MOCK");
+		news.setBrief("MOCK");
+		news.setContent("MOCK");
+		news.setDate(new Date());
+		news.setTitle("MOCK");
+		
+		when(dao.getNewsById(anyInt())).thenReturn(news);
+		when(dao.saveNews(any())).thenReturn(1);
+	}
+	
+	@Test
+	public void testGetNewsById() {
+		
+		News result = testedClass.getNewsById(1);
+		
+		assertEquals(news, result);
+	}
+}
